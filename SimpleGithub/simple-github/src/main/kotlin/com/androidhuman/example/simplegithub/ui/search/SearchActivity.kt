2a8170f478cfe998a7e4650bb8d2ce96f7a11e43
gihop <giho-p@hanmail.net>
2020-07-27 18:23:43 +0900

    SearchActivity도 Call 객체를 CompositeDisposable 객체로 변경한다.

--- SimpleGithub/simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchActivity.kt
+++ SimpleGithub/simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/ui/search/SearchActivity.kt
@@ -1,6 +1,5 @@
 package com.androidhuman.example.simplegithub.ui.search
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -14,6 +13,9 @@ import com.androidhuman.example.simplegithub.api.provideGithubApi
 import com.androidhuman.example.simplegithub.api.model.GithubRepo
 import com.androidhuman.example.simplegithub.api.model.RepoSearchResponse
 import com.androidhuman.example.simplegithub.ui.repo.RepositoryActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_search.*
 import org.jetbrains.anko.startActivity
 import retrofit2.Call
@@ -31,8 +33,9 @@ class SearchActivity : AppCompatActivity(), SearchAdapter.ItemClickListener {
     }
     internal val api by lazy { provideGithubApi(this) }
 
-    //널 값을 허용하도록 한 후, 초기값을 명시적으로 null로 지정한다.
-    internal var searchCall: Call<RepoSearchResponse>? = null
+    //여러 디스포저블 객체를 관리할 수 있는 CompositeDisposable 객체를 초기화한다.
+    //var searchCall: Call<RepoSearchResponse>? = null 대신해서 사용한다.
+    internal val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,8 +51,10 @@ class SearchActivity : AppCompatActivity(), SearchAdapter.ItemClickListener {
 
     override fun onStop() {
         super.onStop()
-        //액티비티가 화면에서 사라지는 시점에 API 호출 객체가 생성되어 있다면 API 요청을 취소한다.
-        searchCall?.run { cancel() }
+
+        //관리하고 있던 디스포저블 객체를 모두 해제한다.
+        //searchCall?.run{ cancel() } 대신 사용한다.
+        disposables.clear()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -110,37 +115,51 @@ class SearchActivity : AppCompatActivity(), SearchAdapter.ItemClickListener {
     }
 
     private fun searchRepository(query: String) {
-        clearResults()
-        hideError()
-        showProgress()
-        searchCall = api.searchRepository(query)
+        //REST API를 통해 검색 결과를 요청한다.
+        disposables.add(api.searchRepository(query)
 
-        //Call 인터페이스를 구현하는 익명 클래스의 인스턴스를 생성한다.
-        //앞에서 API 호출에 필요한 객체를 받았으므로, 이 시점에서 searchCall 객체의 값은 널이 아니다.
-        searchCall!!.enqueue(object : Callback<RepoSearchResponse?> {
-            override fun onResponse(call: Call<RepoSearchResponse?>,
-                                    response: Response<RepoSearchResponse?>) {
-                hideProgress()
-                val searchResult = response.body()
-                if (response.isSuccessful && null != searchResult) {
-                    //with() 함수를 사용하여 adpater 범위 내에서 작업을 수행한다.
-                    with(adapter) {
-                        setItems(searchResult.items)
+                //Observable 형태로 결과를 바꿔주기 위해 flatMap을 사용한다.
+                .flatMap {
+                    if(0 == it.totalCount){
+                        //검색 결과가 없을 경우, 에러를 발생시켜 에러 메시지를 표시하도록 한다.
+                        //곧바로 에러 블록이 실행된다.
+                        Observable.error(IllegalStateException("No search result"))
+                    }
+                    else{
+                        //검색 결과 리스트를 다음 스트림으로 전달한다.
+                        Observable.just(it.items)
+                    }
+                }
+
+                //이 이후에 수행되는 코드는 모두 메인 스레드에서 실행한다.
+                //RxAndroid에서 제공하는 스케줄러인 AndroidSchedulers.mainThread()를 사용한다.
+                .observeOn(AndroidSchedulers.mainThread())
+
+                //구독할 때 수행할 작업을 수행한다.
+                .doOnSubscribe {
+                    clearResults()
+                    hideError()
+                    showProgress()
+                }
+
+                //스트림이 종료될 때 수행할 작업을 구현한다.
+                .doOnTerminate { hideProgress() }
+
+                //옵저버블을 구독한다.
+                .subscribe({ items ->
+
+                    //API를 통해 검색 결과를 정상적으로 받았을 때 처리할 작업을 구현한다.
+                    //작업 중 오류가 발생하면 이 블록은 호출되지 않는다.
+                    with(adapter){
+                        setItems(items)
                         notifyDataSetChanged()
                     }
-                    if (0 == searchResult.totalCount) {
-                        showError(getString(R.string.no_search_result))
-                    }
-                } else {
-                    showError("Not successful: " + response.message())
+                }){
+                    //에러 블록.
+                    //네트워크 오류나 데이터 처리 오류 등 작업이 정상적으로 완료되지 않았을 때 호출한다.
+                    showError(it.message)
                 }
-            }
-
-            override fun onFailure(call: Call<RepoSearchResponse?>, t: Throwable) {
-                hideProgress()
-                showError(t.message)
-            }
-        })
+        )
     }
 
     private fun updateTitle(query: String) {
